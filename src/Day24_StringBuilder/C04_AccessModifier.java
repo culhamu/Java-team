4848d846dfffa8a4446f80ff5ecfce5a1014c6fc
Mustafa Çulha <your_merguven75@mail.com>
2022-12-27 13:50:12 +0300

    24. gün sonu

--- src/Day24_StringBuilder/C04_AccessModifier.java
+++ src/Day24_StringBuilder/C04_AccessModifier.java
@@ -10,5 +10,14 @@ public class C04_AccessModifier {
     //str access modifier i default access modifier
     public static void main(String[] args) {
 
+        C05_Dclass obj1=new C05_Dclass();
+        System.out.println(obj1.isimDefault);//Furkan
+        obj1.defaultStaticOlmayanMethod();//"static olmayan çalıştı"
+
+        System.out.println(obj1.isimDefaultStatic);//Sevilay
+        System.out.println(C05_Dclass.isimDefaultStatic);//Sevilay
+
+       obj1.defaultStaticMethod();//static olan çalıştı
+       C05_Dclass.defaultStaticMethod();//static olan çalıştı
     }
 }
---
+++ src/Day24_StringBuilder/C06_Encaptulation.java
@@ -0,0 +1,17 @@
+package Day24_StringBuilder;
+
+public class C06_Encaptulation {
+    public static void main(String[] args) {
+        /*
+        Access modifiers bir class üyesine erişimi sınırlamdırırken görme yetkisiniz(read) ve
+        değiştirme yetkisini(write) ayırt edemez.
+
+        ama gerçek hayatta buna ihtiyaç vardır
+        Bazı class üyelerinin readable olması lazımken
+        Bazı class üyelerinin writable olması lazım
+        Ve Bazı class üyelerinin her iki yetkiyede sahip olması istenir
+
+        Java bu yetkilendirme işlemini Encaptulation ile çözmüştür
+         */
+    }
+}
