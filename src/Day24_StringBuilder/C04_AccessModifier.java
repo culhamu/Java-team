740325d554b1df099e28e5af96f1d398edf935a7
Mustafa Çulha <your_merguven75@mail.com>
2022-12-27 12:46:27 +0300

    24. gün sonu

---
+++ src/Day24_StringBuilder/C04_AccessModifier.java
@@ -0,0 +1,14 @@
+package Day24_StringBuilder;
+
+public class C04_AccessModifier {
+
+    String str;
+    //Access modifier i gözlerimizle göremiyorsak default modifier vardır
+
+    // bu class da default constructor var
+    //str ın değeri null dır(default deger)
+    //str access modifier i default access modifier
+    public static void main(String[] args) {
+
+    }
+}
