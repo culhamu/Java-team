958aac23820c5d60239a1e18f9dce7a53c95e222
Mustafa Çulha <your_merguven75@mail.com>
2022-12-28 13:31:58 +0300

    25. gün sonu

---
+++ src/Day25_Inheritance/APersonel.java
@@ -0,0 +1,14 @@
+package Day25_Inheritance;
+
+public class APersonel {
+    String isim="İsim atanmadı";
+    String soyIsim="Soyisim atanmadı";
+    String telefon="Telefon atanmadı";
+    public static void main(String[] args) {
+
+    }public  void maas(){
+        System.out.println("Hastanemizde uygulanan asgari ücret 1700 euro dur");
+    }public void ozelSigorta(){
+        System.out.println("Tüm personelin özel sağlık sigortası yaptırılır");
+    }
+}
