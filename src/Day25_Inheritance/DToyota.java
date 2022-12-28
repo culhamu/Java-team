fab5b503d5d3753407e34e3d2a633d2255fe847a
Mustafa Çulha <your_merguven75@mail.com>
2022-12-28 14:47:50 +0300

    25. gün sonu

---
+++ src/Day25_Inheritance/DToyota.java
@@ -0,0 +1,13 @@
+package Day25_Inheritance;
+
+public class DToyota extends CCar {
+    DToyota(){
+
+        System.out.println("Parametresiz Toyota Constructoru çalıştı");
+    }
+    DToyota(String renk){
+        System.out.println("Parametreli toyota cons çalıştı");
+    }
+    String marka="Toyota";
+    String uretimYeri="Toyota farklı ülkelerde üreetilir";
+}
