a2f6a809c09add0ddc1a4ac45195d3ab969e7333
Mustafa Çulha <your_merguven75@mail.com>
2023-01-02 14:49:57 +0300

    27. gun sonu

---
+++ src/Day27_Overriding/GExeptions.java
@@ -0,0 +1,15 @@
+package Day27_Overriding;
+
+public class GExeptions {
+    public static void main(String[] args) {
+        int a=23;
+        int b=0;
+
+        //bu kodda paydanın 0 olması öngörülebilecek bir risktir
+        //kodu yazarken çözülebilecek bir durumdur
+
+        if (b!=0){
+            System.out.println("iki sayının bölümü : "+a/b);
+        }else System.out.println("Bölecek sayı 0 olamaz");
+    }
+}
