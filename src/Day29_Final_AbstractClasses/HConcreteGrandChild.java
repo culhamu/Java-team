87e741b6c3a8f05623d497aa4efe3e0978dad015
Mustafa Çulha <your_merguven75@mail.com>
2023-01-04 14:50:15 +0300

    29. Gün Sonu

---
+++ src/Day29_Final_AbstractClasses/HConcreteGrandChild.java
@@ -0,0 +1,13 @@
+package Day29_Final_AbstractClasses;
+
+public class HConcreteGrandChild extends GAbstractChild{
+    @Override
+    public void method2() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+}
